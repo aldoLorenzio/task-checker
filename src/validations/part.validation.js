b7f2a1e0e83b3840882585aa7d044a060215fbbe
aldoLorenzio <aldolorenzio@gmail.com>
2024-03-19 15:53:57 +0700

    update: part.validation

---
+++ src/validations/part.validation.js
@@ -0,0 +1,48 @@
+const Joi = require('joi');
+const { password, objectId } = require('./custom.validation');
+
+const createPart = {
+  body: Joi.object().keys({
+    partName: Joi.string().required(),
+  }),
+};
+
+const getParts = {
+  query: Joi.object().keys({
+    partName: Joi.string(),
+    sortBy: Joi.string(),
+    limit: Joi.number().integer(),
+    page: Joi.number().integer(),
+  }),
+};
+
+const getPart = {
+  params: Joi.object().keys({
+    partId: Joi.string().custom(objectId),
+  }),
+};
+
+const updatePart = {
+  params: Joi.object().keys({
+    partId: Joi.required().custom(objectId),
+  }),
+  body: Joi.object()
+    .keys({
+      partName: Joi.string().required()
+    })
+    .min(1),
+};
+
+const deletePart = {
+  params: Joi.object().keys({
+    partId: Joi.string().custom(objectId),
+  }),
+};
+
+module.exports = {
+  createPart,
+  getParts,
+  getPart,
+  updatePart,
+  deletePart,
+};
