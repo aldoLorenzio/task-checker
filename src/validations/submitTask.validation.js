99095b84d46f6c718564faaf9a8873a13e556dc0
aldoLorenzio <aldolorenzio@gmail.com>
2024-03-19 15:56:09 +0700

    add: submitTask.validation

---
+++ src/validations/submitTask.validation.js
@@ -0,0 +1,54 @@
+const Joi = require('joi');
+const { password, objectId } = require('./custom.validation');
+
+const createsubmitTask = {
+  body: Joi.object().keys({
+    subjectId: Joi.string().required().custom(objectId),
+    answer: Joi.string().required(),
+    result: Joi.boolean(),
+    userId: Joi.string().required().custom(objectId)
+  }),
+};
+
+const getsubmitTasks = {
+  query: Joi.object().keys({
+    submitTaskName: Joi.string(),
+    sortBy: Joi.string(),
+    limit: Joi.number().integer(),
+    page: Joi.number().integer(),
+  }),
+};
+
+const getsubmitTask = {
+  params: Joi.object().keys({
+    submitTaskId: Joi.string().custom(objectId),
+  }),
+};
+
+const updatesubmitTask = {
+  params: Joi.object().keys({
+    submitTaskId: Joi.required().custom(objectId),
+  }),
+  body: Joi.object()
+    .keys({
+      subjectId: Joi.string().required().custom(objectId),
+      answer: Joi.string().required(),
+      result: Joi.boolean(),
+      userId: Joi.string().required().custom(objectId)
+    })
+    .min(1),
+};
+
+const deletesubmitTask = {
+  params: Joi.object().keys({
+    submitTaskId: Joi.string().custom(objectId),
+  }),
+};
+
+module.exports = {
+  createsubmitTask,
+  getsubmitTasks,
+  getsubmitTask,
+  updatesubmitTask,
+  deletesubmitTask,
+};
